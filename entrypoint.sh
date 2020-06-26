1af758679005ec96bfe20769dcf1493f6abbdc43
Ryan Lieu <github-benzBrake@woai.ru>
2020-06-26 12:34:22 +0800

    ad php extensions support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+# Install External PHP Extensions
+if [ -d /www/php-extensions ]; then
+	cd /www/php-extensions
+       	exts=`ls -al | grep -v ^d | awk '{print $9}'`
+	echo -e "\n" >> /etc/php7/php.ini
+	for e in $exts; do
+		grep "/www/php-extensions/${e}" /etc/php7/php.ini &> /dev/null
+		if [ $? -ne 0 ]; then
+			echo 'extension=/www/php-extensions/'${e} >> /etc/php7/php.ini
+		fi
+	done
+	cd -
+fi
 # Run php
 /usr/sbin/php-fpm7
 # Run caddy
